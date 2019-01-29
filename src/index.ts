10f488524d1a1fa32a9ae8702f8091dc15ab937d
Greg Werner <werner.greg@gmail.com>
2019-01-28 23:38:33 -0500

    dev env setup, publish with travis
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,73 @@
-import {
-  IDisposable, DisposableDelegate
-} from '@phosphor/disposable';
+import { IDisposable, DisposableDelegate } from '@phosphor/disposable';
 
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
 
-import {
-  ToolbarButton, showDialog, Dialog
-} from '@jupyterlab/apputils';
+import { ToolbarButton, showDialog, Dialog } from '@jupyterlab/apputils';
 
-import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-import {
-  NotebookPanel, INotebookModel
-} from '@jupyterlab/notebook';
-
+import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
 
 /**
  * The plugin registration information.
  */
 const plugin: JupyterLabPlugin<void> = {
   activate,
-  id: 'illumidesk_canvas:buttonPlugin',
+  id: 'jupyter-lti:buttonPlugin',
   autoStart: true
 };
-
 
 /**
  * A notebook widget extension that adds a button to the toolbar.
  */
-export
-class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
   assignment_id: string;
   constructor(assignment_id: string) {
     this.assignment_id = assignment_id;
-  };
+  }
   /**
    * Create a new extension object.
    */
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
     let self = this;
     let callback = () => {
-        let url = window.location.pathname.split('endpoint')[0] + "lti/assignment/" + self.assignment_id + "/";
-        let buttons = [Dialog.okButton()];
-        let errorDialog = {
-            title:"Error",
-            body: "There was an error while sending submission",
-            buttons: buttons
-        };
-        context.save();
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({'path': context.path}),
-            headers: {'Content-Type': 'application/json;charset=utf-8'},
-            referrerPolicy: "no-referrer"
-        }).then(res => {
-            if (!res.ok) {
-                console.log(res);
-                showDialog(errorDialog);
-            } else {
-                showDialog({
-                    title: "Success",
-                    body: "Your assignment was sent to Canvas",
-                    buttons: buttons
-                })
-            }
+      let url =
+        window.location.pathname.split('endpoint')[0] +
+        'lti/assignment/' +
+        self.assignment_id +
+        '/';
+      let buttons = [Dialog.okButton()];
+      let errorDialog = {
+        title: 'Error',
+        body: 'There was an error while sending submission',
+        buttons: buttons
+      };
+      context.save();
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({ path: context.path }),
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        referrerPolicy: 'no-referrer'
+      })
+        .then(res => {
+          if (!res.ok) {
+            console.log(res);
+            showDialog(errorDialog);
+          } else {
+            showDialog({
+              title: 'Success',
+              body: 'Your assignment was sent to Canvas',
+              buttons: buttons
+            });
+          }
         })
         .catch(error => {
-            showDialog(errorDialog);
-            console.error(error);
+          showDialog(errorDialog);
+          console.error(error);
         });
     };
     let button = new ToolbarButton({
@@ -97,11 +93,13 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
 function activate(app: JupyterLab) {
   let query = new URLSearchParams(window.location.search);
   let assignment_id = query.get('assignment_id');
-  if(!!assignment_id) {
-    app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension(query.get('assignment_id')));
+  if (!!assignment_id) {
+    app.docRegistry.addWidgetExtension(
+      'Notebook',
+      new ButtonExtension(query.get('assignment_id'))
+    );
   }
-};
-
+}
 
 /**
  * Export the plugin as default.
